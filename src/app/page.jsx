8083bf5d32a917f8b779c2f450a9e7316a86693d
gachagajnr <williampius17@users.noreply.github.com>
2024-02-29 00:58:01 +0300

    setup complete

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Link } from "@nextui-org/react";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="#" size="lg" color="secondary"> 
+        muthomi g pius
+      </Link>
+    </main>
+  );
+}
